77d0937b0369e0bc0fff9202543dbf51a66e9885
Cilia Claij <cclaij@tudelft.nl>
2026-03-25 15:10:16 +0100

    fix navigation issues

--- scripts/Ship.cs
+++ scripts/Ship.cs
@@ -1,21 +1,18 @@
 using Godot;
 using System;
-using System.Drawing;
-using System.IO;
-using System.Runtime.InteropServices;
 
 public partial class Ship : CharacterBody3D
 {
 	[Export]
 	private World _world;
 
-	private float _movementSpeed = 1.0f;
+	private float _movementSpeed = 2.0f;
 	private Vector3 _movementTargetPosition = new Vector3(1.0f, 1.0f, 10.0f);
 	private bool _reachedLocalTarget = true;
 	private bool _reachedTarget = false;
 
 	private Vector3 _localVelocity;
-	private Vector2 _localTargetPosition;
+	private Vector2 _localTargetPosition; 
 
 	private NavigationAgent3D _navigationAgent;
 
@@ -49,18 +46,16 @@ public partial class Ship : CharacterBody3D
 
 		if (_reachedTarget)
 		{
-			GD.Print("Ship reached target");
 			// has to wait for new target
 		}
 
 		else if (_reachedLocalTarget)
 		{
-			// GD.Print("going to new local target");
 			Vector3 currentAgentPosition = GlobalTransform.Origin;
 			Vector2 currentPosition = new Vector2(currentAgentPosition.X, currentAgentPosition.Z);
 			long currentId = _world.Navigation.AstarWater.GetClosestPoint(currentPosition);
 
-			Vector2 targetPosition = new Vector2(_movementTargetPosition.X, _movementTargetPosition.Z);
+			Vector2 targetPosition = new Vector2(MovementTarget.X, MovementTarget.Z);
 			long targetId = _world.Navigation.AstarWater.GetClosestPoint(targetPosition);
 			
 			Vector2 astarPosition = _world.Navigation.AstarWater.GetPointPath(currentId, targetId)[1];
@@ -71,14 +66,6 @@ public partial class Ship : CharacterBody3D
 			_localTargetPosition = astarPosition;
 			MoveAndSlide();
 			_reachedLocalTarget = false;
-			GD.Print("Astar path: ");
-			foreach (Vector2 position in _world.Navigation.AstarWater.GetPointPath(currentId, targetId))
-			{
-				GD.Print(position);
-			}
-			GD.Print("current goal:", _world.Navigation.AstarWater.GetPointPath(currentId, targetId)[1], ", ", astarPosition);
-			// GD.Print("current position: ", currentPosition, ", local goal: ", astarPosition, ", local velocity: ", navigationalVelocity);
-			// GD.Print("A star path: ", _world.Navigation.AstarWater.GetPointPath(currentId, targetId)[0], ",", _world.Navigation.AstarWater.GetPointPath(currentId, targetId)[1], ",", _world.Navigation.AstarWater.GetPointPath(currentId, targetId)[2]);
 		}
 		
 		else
@@ -87,16 +74,13 @@ public partial class Ship : CharacterBody3D
 			Vector3 currentAgentPosition = GlobalTransform.Origin;
 			Velocity = _localVelocity;
 			MoveAndSlide();
-			// GD.Print("current pos:", currentAgentPosition, ", target pos: ", _movementTargetPosition);
-			if((Math.Abs(_movementTargetPosition.X - currentAgentPosition.X) < _navigationAgent.TargetDesiredDistance) && (_movementTargetPosition.Z - currentAgentPosition.Z) < _navigationAgent.TargetDesiredDistance)
+			if((Math.Abs(MovementTarget.X - currentAgentPosition.X) < _navigationAgent.TargetDesiredDistance) && (MovementTarget.Z - currentAgentPosition.Z) < _navigationAgent.TargetDesiredDistance)
 			{
 				_reachedTarget = true;
-				GD.Print("Reached Target");
 			}
 			if((Math.Abs(_localTargetPosition.X - currentAgentPosition.X) < _navigationAgent.TargetDesiredDistance) && (_localTargetPosition.Y - currentAgentPosition.Z) < _navigationAgent.TargetDesiredDistance)
 			{
 				_reachedLocalTarget = true;
-				GD.Print("Reached Local Target");
 			}
 		}
 
