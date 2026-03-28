3cd684619292f1809cfabcc2301ce04a8318cdf3
Ward van der Velden <wardvandervelden97@gmail.com>
2026-03-28 11:57:04 +0100

    Resolve merge conflict

--- scripts/World.cs
+++ scripts/World.cs
@@ -6,79 +6,80 @@ using System.Collections.Generic;
 [Tool]
 public partial class World : Node3D
 {
-    #region Properties and fields
+	#region Properties and fields
 
-    /// <summary>
-    /// Tile count along either side of the world
-    /// </summary>
-    public int Size { get; private set; } = 32;
+	/// <summary>
+	/// Tile count along either side of the world
+	/// </summary>
+	public int Size { get; private set; } = 32;
 
-    /// <summary>
-    /// Time in the world [s]
-    /// </summary>
-    public double Time { get; private set; } = 3600.0 * 11.0;
+	/// <summary>
+	/// Time in the world [s]
+	/// </summary>
+	public double Time { get; private set; } = 3600.0 * 11.0;
 
-    /// <summary>
-    /// Strength of the wind [m/s] (This will have a relation to the water height)
-    /// </summary>
-    public double Wind { get; private set; }
+	/// <summary>
+	/// Strength of the wind [m/s] (This will have a relation to the water height)
+	/// </summary>
+	public double Wind { get; private set; }
 
-    /// <summary>
-    /// Directional angle of the wind [rad]
-    /// </summary>
-    public float WindAngle { get; private set; }
+	/// <summary>
+	/// Directional angle of the wind [rad]
+	/// </summary>
+	public float WindAngle { get; private set; }
 
-    public TaskManager TaskManager { get; private set; }
+	public TaskManager TaskManager { get; private set; }
 
-    public Tile[,] _tiles;
-    private List<Building> _buildings = new List<Building>();
-    private List<Entity> _entities = new List<Entity>();
-    public Navigation Navigation { get; set; }
+	public Tile[,] _tiles;
+	private List<Building> _buildings = new List<Building>();
+	private List<Entity> _entities = new List<Entity>();
+	public Navigation Navigation { get; set; }
 
-    private double _waveTimer = 0.0;
-    private double _wavePeriod = 5.0;
+	private double _waveTimer = 0.0;
+	private double _wavePeriod = 5.0;
 
-    private bool _hasTiles = false;
+	private bool _hasTiles = false;
 
-    private Node3D _tilesNode;
-    private Node3D _buildingsNode;
-    private Node3D _entitiesNode;
+	private Node3D _tilesNode;
+	private Node3D _buildingsNode;
+	private Node3D _entitiesNode;
 
-    [Export]
-    private DirectionalLight3D _sun;
+	[Export]
+	private DirectionalLight3D _sun;
 
-    #endregion
-
-
-    #region Instantation methods
-
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-        TaskManager = new TaskManager(this);
-
-        _tilesNode = GetNode<Node3D>("Tiles");
-        _buildingsNode = GetNode<Node3D>("Buildings");
-        _entitiesNode = GetNode<Node3D>("Entities");
-
-        InstantiateWorld();
-    }
+	#endregion
 
 
-    private void InstantiateWorld()
-    {
-        if (_hasTiles) return;
+	#region Instantation methods
 
-        InstantiateTiles();
-        GenerateSimpleWorld();
-        Navigation = new Navigation(this);
-        
-        _hasTiles = true;
-    }
-    
-    private void InstantiateTiles() 
-    {
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		TaskManager = new TaskManager(this);
 
+		_tilesNode = GetNode<Node3D>("Tiles");
+		_buildingsNode = GetNode<Node3D>("Buildings");
+		_entitiesNode = GetNode<Node3D>("Entities");
+
+		InstantiateWorld();
+	}
+
+
+	private void InstantiateWorld()
+	{
+		if (_hasTiles) return;
+
+		InstantiateTiles();
+		GenerateSimpleWorld();
+		Navigation = new Navigation(this);
+
+		_hasTiles = true;
+	}
+
+
+	private void InstantiateTiles()
+	{
+		PackedScene tileScene = GD.Load<PackedScene>("res://scenes/tile.tscn");
 		_tiles = new Tile[Size, Size];
 		for (int y = 0; y < Size; y++)
 		{
@@ -160,7 +161,6 @@ public partial class World : Node3D
 			return;
 		}
 
-		
 		ProcessTime(deltaTime);
 		ProcessWater(deltaTime);
 		Navigation.GenerateWaterGrid();
